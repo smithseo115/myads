8d301f691f31734ab853fa1fa4291566394d66c3
smithseo115 <131514645+smithseo115@users.noreply.github.com>
2025-09-14 13:38:55 +0700

    Update links.js

--- links.js
+++ links.js
@@ -6,22 +6,22 @@
             },
             kaiyun: { 
                 link: "https://www.qvq6mz.vip:7988/register57288/?i_code=72491894",
-                m_link: "https://www.j8h703.vip:7988/entry/register57288/?i_code=72491894",
-                app_link: "https://www.u1fxsy.vip:9077/57288?i_code=72491894",
+                m_link: "https://www.ff88nk.vip:9077/entry/register57288/?i_code=72491894",
+                app_link: "https://www.ui6era.vip:9663/57288?i_code=72491894",
             },
             leyu: {
-                link: "https://www.q2rf2y.vip:9063/register75647/?i_code=4402258",
-                m_link: "https://www.ymjorl.vip:9962/entry/register75647/?i_code=4402258",
-                app_link: "https://www.ktg0g5.vip:3639/75647?i_code=4402258",
+                link: "https://www.a56lzw.vip:4543/register75647/?i_code=4402258",
+                m_link: "https://www.klo7xy.vip:6848/entry/register75647/?i_code=4402258",
+                app_link: "https://www.wtt3n0.vip:9056/75647?i_code=4402258",
             },
             hth: {
                 link: "https://www.2w0orm.vip:3127/register26740/?i_code=3997673",
-                m_link: "https://www.w6jc5r.vip:9964/entry/register26740/?i_code=3997673",
-                app_link: "https://www.ql9flo.vip:3962/26740?i_code=3997673",
+                m_link: "https://www.vfva93.vip:9249/entry/register26740/?i_code=3997673",
+                app_link: "https://www.bscvt6.vip:9964/26740?i_code=3997673",
             },
             jiuyou: {
                 link: "https://www.ontdtq.vip:6022/register76810/?i_code=51975017",
-                m_link: "https://www.pp67i6.vip:6848/entry/register76810/?i_code=51975017",
-                app_link: "https://www.rpijfv.vip:9077/76810?i_code=51975017",
+                m_link: "https://www.ae6jwl.vip:9962/entry/register76810/?i_code=51975017",
+                app_link: "https://www.11xm77.vip:9249/76810?i_code=51975017",
             },
         };
